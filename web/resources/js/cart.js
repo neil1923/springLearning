b44cc835bc74562211e9d9a8092fc9c78052323a
neil1923 <799353209@qq.com>
2017-09-04 22:59:44 +0800

    1.close: 1.1完成对vue.js的代码注释 1.2完成对cart.html使用到vue的命令的注释 2.open： 2.1文件上传没有完成（上传路径格式存在问题） 2.2地址选配注释未完成 2.3购物车完整功能未完成

--- web/resources/js/cart.js
+++ web/resources/js/cart.js
@@ -1,5 +1,8 @@
 var vm = new Vue({
+    //1.vue监听的范围
     el:"#app",
+
+    //2.反向操作jsp的数据
     data:{
         totalMoney: 0,
         productList: [],
@@ -7,18 +10,26 @@ var vm = new Vue({
         delFlag: false,
         curProduct: ''
     },
+
+    //3.过滤器是对数据的格式化
     filters:{
         formatMoney: function(value) {
             return "¥" + value.toFixed(2);
         }
     },
+
+    //4.编译完成后所要默认执行的方法--理解为初始化
     mounted:function () {
         Vue.nextTick(function () {
             vm.cartView();
         });
     },
+
+    //5.方法
     methods:{
         cartView: function () {
+            //this 默认指向的是vue的实例
+            //在方法（function）的内部this作用域要改变 所以将this提取出来
             var _this = this;
             this.$http.get("resources/data/cartData.json").then(function (res){
                 _this.productList = res.data.result.list;
@@ -50,7 +61,7 @@ var vm = new Vue({
             this.calcTotalPrice();
         },
 
-        //全选
+        //是否全选
         checkAll: function(){
             this.checkAllFlag = !this.checkAllFlag;
             var _this = this;
@@ -93,12 +104,12 @@ var vm = new Vue({
                     _this.totalMoney += item.productQuantity*item.productPrice;
                 }
             })
-        } ,
+        },
 
-        //
     }
 });
 
+//全局过滤器
 Vue.filter("money",function(value, type){
     return "￥ "+value.toFixed(2)+type;
 });
